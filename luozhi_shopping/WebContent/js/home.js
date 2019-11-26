e7ad55594e163466c00f7a56d4ea948beaf43ed9
Sjh <1553726543@qq.com>
2019-11-26 11:02:16 +0800

    改了一下

--- luozhi_shopping/WebContent/js/home.js
+++ luozhi_shopping/WebContent/js/home.js
@@ -19,97 +19,97 @@ $('.totop-a').click(function(){
 });
 
 
-//默认要请求热门商品
-(function(){
-var page = 1;
-var pagesize = 8;
-//封装热门商品数据
-function getHotGoodsList(){   
-    //请求数据
-    $.get('http://www.wjian.top/shop/api_goods.php',{
-      page:page,
-      pagesize:pagesize,
-    }, function(result){
-      var obj = JSON.parse(result);
-      //验证
-      if(obj.code != 0){
-        return;
-      };
-      //渲染
-  for(var i = 0; i < obj.data.length; i++){
-    var str = `
-    <div class="col-md-3">
-      <div class="thumbnail">
-	        <a href="product_particular.html?goods_id=${obj.data[i].goods_id}">
-		        <img src="${obj.data[i].goods_thumb}"/>
-		        <caption>
-		          <p class="goods-name">${obj.data[i].goods_name}</p>
-		          <p class="goods-desc">${obj.data[i].goods_desc}</p>
-		        </caption>
-	        </a>
-	        <p class="goods-price">¥${obj.data[i].price}</p>
-      </div>
-    </div>
-    `;
-        //每遍历一次就要去添加一次
-        $('#hot-goodsList').append(str); 
-      };
-      //放开锁
-      lock = false;
-    });
-};
-//刷新就要有商品
-getHotGoodsList();
-var lock = false;
-	
-$('#loading').click(function(){
-	if(lock){return};
-      lock = true;
-      //加载下一页
-      page++;
-      getHotGoodsList();
-});
-
-
-})();
-
-
-//封装推荐商品数据
-function getRecommendGoodsList(){   
-    //请求数据
-  $.get('http://www.wjian.top/shop/api_goods.php',{
-    page:2,
-    pagesize:8,
-  }, function(result){
-  var obj = JSON.parse(result);
-  console.log(obj);
-  //验证
-  if(obj.code != 0){
-    console.log(obj.message);
-    return;
-  };
-  //渲染
-  for(var i = 0; i < obj.data.length; i++){
-    var str = `
-    <div class="col-md-3">
-      <div class="thumbnail">
-        <a href="product_particular.html?goods_id=${obj.data[i].goods_id}">
-	        <img src="${obj.data[i].goods_thumb}"/>
-	        <caption>
-	          <p class="goods-name">${obj.data[i].goods_name}</p>
-	          <p class="goods-desc">${obj.data[i].goods_desc}</p>
-	        </caption>
-        </a>
-        <p class="goods-price">¥${obj.data[i].price}</p>
-      </div>
-    </div>
-    `;
-    //每遍历一次就要去添加一次
-    $('#recommend-goodsList').append(str); 
-      };
-    });
-  };
-  //刷新就要有商品
-  getRecommendGoodsList();
+////默认要请求热门商品
+//(function(){
+//var page = 1;
+//var pagesize = 8;
+////封装热门商品数据
+//function getHotGoodsList(){   
+//    //请求数据
+//    $.get('http://www.wjian.top/shop/api_goods.php',{
+//      page:page,
+//      pagesize:pagesize,
+//    }, function(result){
+//      var obj = JSON.parse(result);
+//      //验证
+//      if(obj.code != 0){
+//        return;
+//      };
+//      //渲染
+//  for(var i = 0; i < obj.data.length; i++){
+//    var str = `
+//    <div class="col-md-3">
+//      <div class="thumbnail">
+//	        <a href="product_particular.html?goods_id=${obj.data[i].goods_id}">
+//		        <img src="${obj.data[i].goods_thumb}"/>
+//		        <caption>
+//		          <p class="goods-name">${obj.data[i].goods_name}</p>
+//		          <p class="goods-desc">${obj.data[i].goods_desc}</p>
+//		        </caption>
+//	        </a>
+//	        <p class="goods-price">¥${obj.data[i].price}</p>
+//      </div>
+//    </div>
+//    `;
+//        //每遍历一次就要去添加一次
+//        $('#hot-goodsList').append(str); 
+//      };
+//      //放开锁
+//      lock = false;
+//    });
+//};
+////刷新就要有商品
+//getHotGoodsList();
+//var lock = false;
+//	
+//$('#loading').click(function(){
+//	if(lock){return};
+//      lock = true;
+//      //加载下一页
+//      page++;
+//      getHotGoodsList();
+//});
+//
+//
+//})();
+//
+//
+////封装推荐商品数据
+//function getRecommendGoodsList(){   
+//    //请求数据
+//  $.get('http://www.wjian.top/shop/api_goods.php',{
+//    page:2,
+//    pagesize:8,
+//  }, function(result){
+//  var obj = JSON.parse(result);
+//  console.log(obj);
+//  //验证
+//  if(obj.code != 0){
+//    console.log(obj.message);
+//    return;
+//  };
+//  //渲染
+//  for(var i = 0; i < obj.data.length; i++){
+//    var str = `
+//    <div class="col-md-3">
+//      <div class="thumbnail">
+//        <a href="product_particular.html?goods_id=${obj.data[i].goods_id}">
+//	        <img src="${obj.data[i].goods_thumb}"/>
+//	        <caption>
+//	          <p class="goods-name">${obj.data[i].goods_name}</p>
+//	          <p class="goods-desc">${obj.data[i].goods_desc}</p>
+//	        </caption>
+//        </a>
+//        <p class="goods-price">¥${obj.data[i].price}</p>
+//      </div>
+//    </div>
+//    `;
+//    //每遍历一次就要去添加一次
+//    $('#recommend-goodsList').append(str); 
+//      };
+//    });
+//  };
+//  //刷新就要有商品
+//  getRecommendGoodsList();
   
 
