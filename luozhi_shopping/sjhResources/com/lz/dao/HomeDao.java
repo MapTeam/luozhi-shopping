52c384d52fa6db802f66310d98730819798b964a
zhouyudong6 <18674628586@163.com>
2019-11-27 09:28:18 +0800

    主页的展示

--- luozhi_shopping/sjhResources/com/lz/dao/HomeDao.java
+++ luozhi_shopping/sjhResources/com/lz/dao/HomeDao.java
@@ -6,4 +6,10 @@ import com.lz.pojo.Goods;
 
 public interface HomeDao {
 	public List<Goods> getHomeGoods();
+	public List<Goods> getHotGoods();
+	public List<Goods> getRecommandGoods();
+	public List<Goods> getIpGoods();
+	public List<Goods> getShuMaGoods();
+	
+	
 }
--- luozhi_shopping/sjhResources/com/lz/dao/impl/HomeDaoImpl.java
+++ luozhi_shopping/sjhResources/com/lz/dao/impl/HomeDaoImpl.java
@@ -13,10 +13,11 @@ import com.lz.pojo.Goods;
 
 public class HomeDaoImpl implements HomeDao {
 
+
 	@Override
 	public List<Goods> getHomeGoods() {
 		List<Goods> gs = new ArrayList<Goods>();
-		Connection conn = DBConnection1.getConnection();
+		Connection conn=DBConnection1.getConnection();
 		String sql = "SELECT * FROM goods ORDER BY hot DESC LIMIT 0,20";
 		try {
 			PreparedStatement ps = conn.prepareStatement(sql);
@@ -43,6 +44,141 @@ public class HomeDaoImpl implements HomeDao {
 			e.printStackTrace();
 		}
 		return gs;
+	}
+
+	@Override
+	public List<Goods> getHotGoods() {
+		// TODO Auto-generated method stub
+		List<Goods> hs= new ArrayList<Goods>();	
+		Connection conn=DBConnection1.getConnection();
+		
+		String sql = "SELECT * FROM goods ORDER BY hot DESC LIMIT 50,16";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				Goods goods = new Goods();
+				goods.setCategory1(rs.getInt("category1"));
+				goods.setCategory2(rs.getInt("category2"));
+				goods.setGbrand(rs.getString("gbrand"));
+				goods.setGcolorid(rs.getInt("gcolorid"));
+				goods.setGcount(rs.getInt("gcount"));
+				goods.setGgrade(rs.getInt("ggrade"));
+				goods.setGid(rs.getInt("gid"));
+				goods.setGintroduce(rs.getString("gintroduce"));
+				goods.setGmaterial(rs.getString("gmaterial"));
+				goods.setGname(rs.getString("gname"));
+				goods.setGpicture(rs.getString("gpicture"));
+				goods.setGprice(rs.getFloat("gprice"));
+				goods.setHot(rs.getInt("hot"));
+				goods.setZpicture(rs.getString("zpicture"));
+				hs.add(goods);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return hs;
+		
+	}
+
+	@Override
+	public List<Goods> getRecommandGoods() {
+		// TODO Auto-generated method stub
+		List<Goods> res = new ArrayList<Goods>();
+		Connection conn=DBConnection1.getConnection();
+	String sql = "SELECT * FROM goods ORDER BY  RAND() LIMIT 20;";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				Goods goods = new Goods();
+				goods.setCategory1(rs.getInt("category1"));
+				goods.setCategory2(rs.getInt("category2"));
+				goods.setGbrand(rs.getString("gbrand"));
+				goods.setGcolorid(rs.getInt("gcolorid"));
+				goods.setGcount(rs.getInt("gcount"));
+				goods.setGgrade(rs.getInt("ggrade"));
+				goods.setGid(rs.getInt("gid"));
+				goods.setGintroduce(rs.getString("gintroduce"));
+				goods.setGmaterial(rs.getString("gmaterial"));
+				goods.setGname(rs.getString("gname"));
+				goods.setGpicture(rs.getString("gpicture"));
+				goods.setGprice(rs.getFloat("gprice"));
+				goods.setHot(rs.getInt("hot"));
+				goods.setZpicture(rs.getString("zpicture"));
+				res.add(goods);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return res;
+
+	}
+
+	@Override
+	public List<Goods> getIpGoods() {
+		// TODO Auto-generated method stub
+		List<Goods> Is = new ArrayList<Goods>();
+		Connection conn=DBConnection1.getConnection();
+		String sql = "SELECT * FROM goods where category1=1008002";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				Goods goods = new Goods();
+				goods.setCategory1(rs.getInt("category1"));
+				goods.setCategory2(rs.getInt("category2"));
+				goods.setGbrand(rs.getString("gbrand"));
+				goods.setGcolorid(rs.getInt("gcolorid"));
+				goods.setGcount(rs.getInt("gcount"));
+				goods.setGgrade(rs.getInt("ggrade"));
+				goods.setGid(rs.getInt("gid"));
+				goods.setGintroduce(rs.getString("gintroduce"));
+				goods.setGmaterial(rs.getString("gmaterial"));
+				goods.setGname(rs.getString("gname"));
+				goods.setGpicture(rs.getString("gpicture"));
+				goods.setGprice(rs.getFloat("gprice"));
+				goods.setHot(rs.getInt("hot"));
+				goods.setZpicture(rs.getString("zpicture"));
+				Is.add(goods);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return Is;
+	}
+
+	@Override
+	public List<Goods> getShuMaGoods() {
+		// TODO Auto-generated method stub
+		List<Goods> sms = new ArrayList<Goods>();
+		Connection conn=DBConnection1.getConnection();
+		String sql = "SELECT * FROM goods where category1=101000";
+		try {
+			PreparedStatement ps = conn.prepareStatement(sql);
+			ResultSet rs = ps.executeQuery();
+			while(rs.next()){
+				Goods goods = new Goods();
+				goods.setCategory1(rs.getInt("category1"));
+				goods.setCategory2(rs.getInt("category2"));
+				goods.setGbrand(rs.getString("gbrand"));
+				goods.setGcolorid(rs.getInt("gcolorid"));
+				goods.setGcount(rs.getInt("gcount"));
+				goods.setGgrade(rs.getInt("ggrade"));
+				goods.setGid(rs.getInt("gid"));
+				goods.setGintroduce(rs.getString("gintroduce"));
+				goods.setGmaterial(rs.getString("gmaterial"));
+				goods.setGname(rs.getString("gname"));
+				goods.setGpicture(rs.getString("gpicture"));
+				goods.setGprice(rs.getFloat("gprice"));
+				goods.setHot(rs.getInt("hot"));
+				goods.setZpicture(rs.getString("zpicture"));
+				sms.add(goods);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return sms;
 	}
 
 }
