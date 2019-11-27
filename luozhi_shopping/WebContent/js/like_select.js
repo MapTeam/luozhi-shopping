aa119768123e8981ba5240c0547e3da42c686edf
huhui <1500908247@qq.com>
2019-11-27 09:26:31 +0800

    胡辉加了模糊查询和分页

---
+++ luozhi_shopping/WebContent/js/like_select.js
@@ -0,0 +1,45 @@
+(function(){
+	//回到顶部
+$(document).scroll(function(){
+ 	
+		if($(document).scrollTop() <800)
+		{	
+			if($('.totop').is(':animated'))
+			{return;}
+			$('.zTi-rigth').css('height','300px');
+			$('.totop').fadeOut(1000);
+		}else{
+			if($('.totop').is(':animated'))
+			{return;}
+			$('.zTi-rigth').css('height','335px');
+			$('.totop').fadeIn(1000);
+		}		
+	});
+	//回到顶部添加点击事件
+	$('.totop').click(function(){
+		$('body,html').animate({'scrollTop':0},1000);
+	});
+})();
+(function() {
+	var num=1;
+	$('#tiaozhuaninput').keyup(function() {
+		var code=/[^\d.]/g;
+		if(code.test($(this).val())==true){
+			$('#tiaozhuaninput').val(num);
+		}else{
+			var max=$('#maxpage').text();
+			num=$('#tiaozhuaninput').val();		
+//			alert(max);
+			if (num>max) {
+				$('#tiaozhuaninput').val(max);
+				num=max;
+			}	
+		}
+		if(event.keyCode==13){
+			window.location.href="LikeSelectServlet?pageNo="+num+"&pageSize="+20+"&val="+$('#selecttitle').text();
+		};
+	});
+//	$('#tiaozhuaninput').blur(function() {
+//		window.location.href="LikeSelectServlet?pageNo="+num+"&pageSize="+20+"&val="+$('#selecttitle').text();
+//	});
+})();
