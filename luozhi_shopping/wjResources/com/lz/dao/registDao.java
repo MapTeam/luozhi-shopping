e2713b752d4487188f43f3b8e125a8da7079d536
wj <616255243@qq.com>
2019-12-03 21:37:27 +0800

    用户删除订单，点击图片跳转详情界面

--- luozhi_shopping/wjResources/com/lz/dao/registDao.java
+++ luozhi_shopping/wjResources/com/lz/dao/registDao.java
@@ -19,5 +19,4 @@ public interface registDao {
 	public boolean insertOutGoodsOrder(Connection conn,String outgoodsname,int uid,String receiveaddress,String tel);
 	public boolean updateGoodOrder(Connection conn,String outgoodname, int status,int goid);
 	public List<GoodsOrderDto> selectOrderByOrSta(int status);
-	public boolean updateOutGoodAddress(int orid);
 }
