41608b6ce37496924e0b2ea921770b539df1947e
Sjh <1553726543@qq.com>
2019-12-03 14:22:23 +0800

    测试webservice

--- luozhi_shopping/sjhResources/com/lz/test01/Test.java
+++ luozhi_shopping/sjhResources/com/lz/test01/Test.java
@@ -11,7 +11,8 @@ public class Test {
 
 	public static void main(String[] args) throws RemoteException {
 		Bank bank = new BankProxy();
-		System.out.println(bank.verifyBankAccount("1234565555").getMsg());
+//		System.out.println(bank.verifyBankAccount("1234565555").getMsg());
+		System.out.println(bank.transferAccounts("123456", "123", "6217002990107872941", 710).getMsg());
 		
 	}
 
