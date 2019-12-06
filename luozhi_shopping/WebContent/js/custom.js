43b98abc3021275eee0905ba025f91bd28157d1b
zhouyudong6 <18674628586@163.com>
2019-12-06 11:02:51 +0800

    500页面

---
+++ luozhi_shopping/WebContent/js/custom.js
@@ -0,0 +1,26 @@
+﻿
+(function ($) {
+    "use strict";
+    var mainApp = {
+
+        main_fun: function () {
+           
+            var count = new countUp("error-link", 10, 500, 0, 5); 
+
+            window.onload = function () {
+                        count.start();
+            }
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
